9239f88c97fa5f4f13a35f5eaac54003d3cbfddb
Redhotsmasher <Red_hot_smasher@yahoo.com>
2015-11-22 16:03:49 +0100

    first commit

---
+++ tests/Test2.c
@@ -0,0 +1,46 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    pthread_mutex_t lock;
+    int value;
+}SharedInt;
+
+sem_t sem;
+SharedInt* sip;
+
+void plus(int* v2) {
+    pthread_mutex_lock(&(sip->lock));
+    sip->value = sip->value + *v2;
+    pthread_mutex_unlock(&(sip->lock));
+}
+
+void *functionWithCriticalSection(int* v2) {
+    // Do some work
+    	// Critical section
+        plus(v2);
+        // End of critical section
+    // Do some more work
+    sem_post(&sem);
+}
+
+int main() {
+    sem_init(&sem, 0, 0);
+    SharedInt si;
+    sip = &si;
+    sip->value = 0;
+    int v2 = 1;
+    pthread_mutex_init(&(sip->lock), NULL);
+    pthread_t thread1;
+    pthread_t thread2;
+    pthread_create (&thread1,NULL,functionWithCriticalSection,&v2);
+    pthread_create (&thread2,NULL,functionWithCriticalSection,&v2);
+    sem_wait(&sem);
+    sem_wait(&sem);
+    pthread_mutex_destroy(&(sip->lock));
+    sem_destroy(&sem);
+    printf("%d\n", sip->value); // Should print "2".
+    return 0;
+}
---
+++ tests/Test3.c
@@ -0,0 +1,74 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+pthread_mutex_t lock;
+pthread_mutex_t lock2;
+int value;
+int value2;
+
+sem_t sem;
+
+void *functionWithCriticalSection(int* v2) {
+    // Do some work
+    pthread_mutex_lock(&(lock));
+    value = value + *v2;
+    pthread_mutex_unlock(&(lock));
+    // Do some more work
+    sem_post(&sem);
+}
+
+void *functionWithOtherCriticalSection(int* v2) {
+    // Do some work
+    pthread_mutex_lock(&(lock2));
+    value2 = value2 + *v2;
+    pthread_mutex_unlock(&(lock2));
+    // Do some more work
+    sem_post(&sem);
+}
+
+void *functionWithMultilockCriticalSection(int* v2) {
+    // Do some work
+    pthread_mutex_lock(&(lock));
+    pthread_mutex_lock(&(lock2));
+    value = value2 + *v2;
+    pthread_mutex_unlock(&(lock));
+    pthread_mutex_unlock(&(lock2));
+    // Do some more work
+    sem_post(&sem);
+}
+
+void *functionWithReadingCriticalSection(int* v2) {
+    // Do some work
+    pthread_mutex_lock(&(lock));
+    printf("%d\n", value);
+    pthread_mutex_unlock(&(lock));
+    // Do some more work
+    sem_post(&sem);
+}
+
+int main() {
+    sem_init(&sem, 0, 0);
+    value = 0;
+    value2 = 0;
+    int v2 = 1;
+    pthread_mutex_init((&(lock)), NULL);
+    pthread_mutex_init((&(lock2)), NULL);
+    pthread_t thread1;
+    pthread_t thread2;
+    pthread_create (&thread1,NULL,functionWithCriticalSection,&v2);
+    pthread_create (&thread2,NULL,functionWithOtherCriticalSection,&v2);
+    sem_wait(&sem);
+    sem_wait(&sem);
+    pthread_create (&thread1,NULL,functionWithMultilockCriticalSection,&v2);
+    pthread_create (&thread2,NULL,functionWithReadingCriticalSection,&v2);
+    sem_wait(&sem);
+    sem_wait(&sem);
+    pthread_mutex_destroy(&(lock));
+    pthread_mutex_destroy(&(lock2));
+    sem_destroy(&sem);
+    printf("%d\n", value); // Should print "2".
+    printf("%d\n", value2); // Should print "1".
+    return 0;
+}
